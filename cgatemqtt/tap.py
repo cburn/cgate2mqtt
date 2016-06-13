023834fd0f02be04ee5ffee9145982fe0de6fc27
Chris Burn <chrisburn@fastmail.net>
2016-06-13 15:34:06 +0100

    Added filtering logger

--- cgatemqtt/tap.py
+++ cgatemqtt/tap.py
@@ -2,8 +2,12 @@ from twisted.python import usage
 from twisted.application import service
 from twisted.internet import reactor
 from twisted.internet.endpoints import clientFromString
+from twisted.logger import LogLevel, FilteringLogObserver, textFileLogObserver, LogLevelFilterPredicate
 from mqtt.client.factory import MQTTFactory
 import cgatemqtt
+
+import sys
+
 
 class Options(usage.Options):
     optParameters = [
@@ -29,3 +33,11 @@ def makeService(config):
     mqtt_service.setCGateService(cgate_service)
 
     return application
+
+def FilteringLog():
+    config = Options()
+    config.parseOptions()
+
+    isLevel = LogLevelFilterPredicate(LogLevel.levelWithName(config['loglevel']))
+    lo = FilteringLogObserver(observer=textFileLogObserver(sys.stdout), predicates=[isLevel])
+    return lo
